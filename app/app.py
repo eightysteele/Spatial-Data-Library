9f1cedbf082fd41f08aa9a0fd774ba996cffefe9
Aaron Steele <eightysteele@gmail.com>
2011-02-07 11:37:38 -0800

    New Variable kind and REST API for metadata

--- app/app.py
+++ app/app.py
@@ -4,6 +4,7 @@ from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 import os
 import logging
+import datetime
 
 # ==============================================================================
 # Datastore model
@@ -23,6 +24,30 @@ class CellIndex(db.Expando):
     """Cell index."""
     value = db.IntegerProperty()
     variable = db.StringProperty()
+    metadata = db.TextProperty() # JSON .hdr info
+
+class Variable(db.Expando):
+    """Variable metadata."""
+    name = db.StringProperty()
+
+def _getprops(obj):
+    """Returns dictionary of all static and dynamic properties of an entity."""
+    dict = {}
+    for key in obj.properties().keys():
+        val = obj.properties()[key].__get__(obj, CellIndex)
+        if type(val) is datetime.datetime:
+            dict[key] = str(val)
+        else:
+            dict[key] = val
+    for key in obj.dynamic_properties():
+        val = obj.__getattribute__(key)
+        if type(val) is datetime.datetime:
+            dict[key] = str(val)
+        else:
+            dict[key] = val
+    dict['key'] = obj.key().name()
+    return dict
+
 
 # ==============================================================================
 # Request handlers
@@ -33,31 +58,29 @@ class BaseHandler(webapp.RequestHandler):
         path = os.path.join(os.path.dirname(__file__), "templates", file)
         self.response.out.write(template.render(path, template_args))
 
-class SandboxHandler(BaseHandler):
-    """Handler for the sandbox stuff."""
+class DataListHandler(BaseHandler):
+    '''Lists all Variable entities as JSON.'''
+    def get(self):
+        self.response.out.write(simplejson.dumps([{_getprops(x)['key']: _getprops(x)} for x in Variable.all()]))
 
-    def _getprops(self, obj):
-        """Returns dictionary of all static and dynamic properties of an entity."""
-        dict = {}
-        for key in obj.properties().keys():
-            dict[key] = obj.properties()[key].__get__(obj, CellIndex)
-        for key in obj.dynamic_properties():
-            logging.info(key)
-            dict[key] = obj.__getattribute__(key)
-        dict['key'] = str(obj.key())
-        return dict
+class DataHandler(BaseHandler):
+    '''Lists metadata for a single Variable entity as JSON.'''
+    def get(self, variable):
+        entity = Variable.get_by_key_name(variable)
+        if entity is None:
+            self.error(404)
+            return
+        self.response.out.write(simplejson.dumps(_getprops(entity)))
+
+class ApiHandler(BaseHandler):
+    """API handler."""
 
     def get(self):
-        """Prototyping cell API stuff..."""
-
         action = self.request.get('action', None)
         if not action:
             self.error(404)
-
+            return
         if action == 'search':
-            # Handles something like:
-            # ?action=search&within=1&value=9&variable=bio1
-
             within = self.request.get('within', None)
             variable = self.request.get('variable', None)
             value = int(self.request.get('pivot', None))
@@ -71,26 +94,11 @@ class SandboxHandler(BaseHandler):
                 self.error(404)
                 return
             cell = db.get(index.parent())
-            self.response.out.write(simplejson.dumps(self._getprops(cell)))
+            self.response.out.write(simplejson.dumps(_getprops(cell)))
 
-        if action == 'create_test_data':
-            """Just creates a test entity in the datastore for bio1.
-                
-            Variable  = BIO1 = Annual Mean Temperature
-            MinValue -50
-            MaxValue 293
-            """
-            val = 10
-            cell = Cell(key_name='f-i-j',
-                        json=simplejson.dumps({'variable':'bio1', 'value':10}))
-            cell_key = cell.put()
-            cell_index = CellIndex(parent=cell_key, variable='bio1', value=val)
-            cell_index.within_1 = [9, 10, 11]
-            cell_index.within_10 = range(val - 10, val + 10 + 1)
-            cell_index.put()
-            self.response.out.write(simplejson.dumps(self._getprops(cell_index)))
-
-application = webapp.WSGIApplication([('/', SandboxHandler), ], debug=True)
+application = webapp.WSGIApplication([('/data/api', ApiHandler),
+                                      ('/data/([\w]*)', DataHandler),
+                                      ('/data', DataListHandler), ], debug=True)
 
 def main():
     run_wsgi_app(application)
