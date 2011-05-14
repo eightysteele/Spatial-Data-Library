0040220e6010b9f0d0ed3908259ba68465866b5e
Aaron <eightysteele@gmail.com>
2011-05-14 11:47:09 -0700

    Modified to bulkload a directory of CSV files to CouchDB using the bulk update endpoint.

--- tools/python/var2cdb.py
+++ tools/python/var2cdb.py
@@ -4,6 +4,7 @@ import csv
 import hashlib
 import logging
 from optparse import OptionParser
+import os
 import re
 import simplejson
 from uuid import uuid4
@@ -17,6 +18,7 @@ DB_CACHE_TABLE = 'cache'
 DB_TMP_TABLE = 'tmp'
 VAR_MIN = -50
 VAR_MAX = 293
+VAR_NODATA = -9999
 
 def scaleval(val, varmin, varmax):
     return varmin + val * (varmax - varmin) / 255 
@@ -25,39 +27,40 @@ def within_list(val, within):
     x = int(val)
     return range(x - within, x + within + 1)
 
-def load(csvfile, couchdb_url):
+def load(csvdir, couchdb_url):
+    csvdir = os.path.abspath(csvdir)
     server = Server(couchdb_url)
-    sdl = server['sdl']
-    dr = csv.DictReader(open(csvfile, 'r'), skipinitialspace=True)
-
-    for row in dr:
-        cell_value = scaleval(float(row['avg_Band1']), VAR_MIN, VAR_MAX)
-        if cell_value > VAR_MAX or cell_value < VAR_MIN:
-            continue
-        doc = {
-            '_id': row['CellKey'],
-            'coords': [], # TODO
-            'vars': {
-                row['RID'].split('_')[0]: float(row['avg_Band1'])
-                }
-            }
-        sdl.save(doc)
+    sdl = server['sdl']    
+    csvfiles = [x for x in os.listdir(csvdir) if x.endswith('.csv')]
+    for csvfile in csvfiles:
+        dr = csv.DictReader(open(os.path.join(csvdir, csvfile), 'r'))
+        docs = []
+        for row in dr:
+            cell_value = scaleval(float(row['avg_Band1']), VAR_MIN, VAR_MAX)
+            if cell_value > VAR_MAX or cell_value < VAR_MIN or cell_value is VAR_NODATA:
+                continue
+            docs.append({                
+                    '_id': row['CellKey'],
+                    'coords': [], # TODO
+                    'vars': {row['RID'].split('_')[0]: float(row['avg_Band1'])}
+                    })
+        sdl.update(docs)        
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)    
     
     # Parses command line parameters:
     parser = OptionParser()
-    parser.add_option("-f", "--csvfile", dest="csvfile",
-                      help="The CSV file",
+    parser.add_option("-d", "--csvdir", dest="csvdir",
+                      help="The directory of CSV files",
                       default=None)
     parser.add_option("-u", "--url", dest="url",
                       help="The CouchDB URL",
                       default=None)
 
     (options, args) = parser.parse_args()
-    csvfile = options.csvfile
+    csvdir = options.csvdir
     url = options.url
     
     # Writes the CSV file:
-    load(csvfile, url)
+    load(csvdir, url)
