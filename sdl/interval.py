5ef033654480dabc5222234012f55ec77954c5a9
John Wieczorek <tuco@berkeley.edu>
2011-08-12 02:25:55 -0700

    Added function to get the interval lists given min and max variable values and r ange to search in. Added unit testing. Fixed bug in get_index_intervals.

--- sdl/interval.py
+++ sdl/interval.py
@@ -17,49 +17,136 @@
 __author__ = "Aaron Steele, Dave Vieglais, and John Wieczorek"
 
 import math
-
-def get_n(min,max):
-    return int(math.ceil(math.log(math.ceil(max) - math.floor(min),2))+1)
-
-def width(n):
-    return int(math.pow(2, n))
-
-def start(val,min,max,n):
-    a = (val-min)
-    b = (val-min)/(math.pow(2,n))
-    c = int(math.floor((val-min)/(math.pow(2,n))))
-    d = c + min
-    while d <= val:
-        d+=int(math.pow(2,n))
-    if d==val:
-        return d
-    return d-int(math.pow(2,n))
  
 def get_indexes(val,min,max,res):
+    newval = int(math.floor(val/res))
     newmin = int(math.floor(min/res))
     newmax = int(math.ceil(max/res))
-    category_count = get_n(newmin,newmax)
+    category_count = int(math.ceil(math.log(math.ceil(newmax) - math.floor(newmin),2)))
     intervals = []
     for n in range(category_count):
-        index = start(val,newmin,newmax,n)
-        print 'start %s end %s' % (index, index + int(pow(2,n)))
+        index = newval-(newval-newmin)%(pow(2,n))
+#        print 'n %s start %s end %s' % (n, index, index + int(pow(2,n)))
         intervals.append(index)
     return intervals
 
 def get_index_intervals(val,min,max,res):
+    ''' Returns a dictionary of interval:values where the values are the starting integers of a range containing val.
+
+        Arguments:
+            val - the value for which to create the interval dictionary
+            min - the minimum value of the variable range, starting value for the intervals for a variable (e.g., -454m for altitude) 
+            max - the maximum value of the variable range (e.g., 8850m for altitude)
+            res - the resolution of the desired intervals (e.g., 1 means every meter, 10 means every 10 meters)
+            
+        Example:
+            Altitudes between 315 and 330 (greater than or equal to 315, less than 330) using resolution=1.
+            i0=315 [315,316} 1
+            i1=316 [316,318} 2
+            i2=318 [318,322} 4
+            i3=322 [322,330} 8          
+
+            Altitudes between 315 and 330 (greater than or equal to 315, less than 330) using resolution=1.
+            i0=32 [320,330} 1
+            i1=32 [320,340} 2
+            i2=30 [300,340} 4
+            i3=26 [260,340} 8
+            i4=18 [180,340} 16
+            i5=18 [180,500} 32
+            i6=18 [180,820} 64
+            i7=-46 [-460,820} 128
+            i8=-46 [-460,2100} 256
+            i9=-46 [-460,4660} 512
+    '''
     intervals = get_indexes(val,min,max,res)
     indexes = dict()
-    a = 0
+    j = 0
     for i in intervals:
-        index = 'i%s' % str(a) 
+        index = 'i%s' % str(j) 
         indexes[index]=i
-        a += 1
+        j += 1
     return indexes
 
+def get_query_intervals(min, max, gte, lt):
+    ''' Returns a dictionary of interval:value-lists that need to be queried to get cells having a variable value within a given range.
+
+        Arguments:
+            min - the minimum value of the variable range, starting value for the intervals for a variable (e.g., -454m for altitude) 
+            max - the maximum value of the variable range (e.g., 8850m for altitude)
+            gte - the bottom end of the range of values to include in the results
+            lt - an integer one greater than the top end of the range of values include in results
+            
+        Example:
+            Altitudes between 315 and 330 (greater than or equal to 315, less than 329).
+            i0=315,328 [315,316} and [328,329} 1
+            i1=316,326 [316,318} and [326,328} 2
+            i2=318,322 [318,322} and [322,326} 4
+    '''
+    ''' Clamp gte and lt to min and max values of the variable.'''
+    if gte<min:
+        gte=min
+    if lt>max:
+        lt=max
+        
+    indexes = dict()
+    ''' Power of 2 for the difference between max and min.'''
+    n = int(math.floor(math.log( (lt-gte), 2)))
+    ''' m is the magic starting value between min and max.''' 
+    m = min+int(math.pow(2,n)*int(math.floor((lt-min)/math.pow(2,n))))
+    ''' top is the difference between m and the top of the range.'''
+    top = lt-m
+    ''' bottom is the difference between m and the bottom of the range.'''
+    bottom = m-gte
+    diff = bottom
+    ''' Iterate through the differences from m to the bottom of the range.'''
+    while diff > 0:
+        n = int(math.floor(math.log( diff, 2)))
+        m=m-int(math.pow(2,n))
+        ''' Add the value to the list of values for the interval.
+            Create the list if there is not one already.'''
+        if not indexes.has_key('i%s' % n):
+            indexes['i%s' % n]=[m]
+        else:
+            l=indexes['i%s' % n]
+            l.append(m)
+            indexes['i%s' % n]=l
+        diff = m-gte
+    m=lt-top
+    diff = top
+    ''' Iterate through the differences from m to the top of the range.'''
+    while diff > 0:
+        n = int(math.floor(math.log( diff, 2)))
+        ''' Add the value to the list of values for the interval.
+            Create the list if there is not one already.'''
+        if not indexes.has_key('i%s' % n):
+            indexes['i%s' % n]=[m]
+        else:
+            l=indexes['i%s' % n]
+            l.append(m)
+            indexes['i%s' % n]=l
+        m=m+int(math.pow(2,n))
+        diff = lt-m
+    return indexes    
+    
 def main():
+    indexes = get_index_intervals(14,-20,40,1)
+    print indexes
+
     indexes = get_index_intervals(320,-454,8550,1)
     print indexes
 
+    indexes = get_index_intervals(320,-454,8550,10)
+    print indexes
+
+    intervals = get_query_intervals(-454, 8850, 315, 329)
+    print intervals
+
+    intervals = get_query_intervals(-20, 40, 10, 20)
+    print intervals
+    
+    intervals = get_query_intervals(-20, 40, -21, 41)
+    print intervals
+    
 if __name__ == "__main__":
     main()
 
---
+++ test/intervaltest.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+
+# Copyright 2011 University of California at Berkeley
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+__author__ = "Aaron Steele and John Wieczorek"
+
+"""This module provides unit testing for Bounding Box class."""
+
+import logging
+import sys
+import unittest
+
+sys.path.insert(0, '../')
+
+from sdl.interval import *
+
+class IntervalTest(unittest.TestCase):
+    def test_index_intervals(self):
+        indexes = get_index_intervals(14,-20,40,1)
+        self.assertEqual(indexes['i0'], 14)
+        self.assertEqual(indexes['i1'], 14)
+        self.assertEqual(indexes['i2'], 12)
+        self.assertEqual(indexes['i3'], 12)
+        self.assertEqual(indexes['i4'], 12)
+        self.assertEqual(indexes['i5'], 12)
+        
+        indexes = get_index_intervals(17.3,-20,40,1)
+        self.assertEqual(indexes['i0'], 17)
+        self.assertEqual(indexes['i1'], 16)
+        self.assertEqual(indexes['i2'], 16)
+        self.assertEqual(indexes['i3'], 12)
+        self.assertEqual(indexes['i4'], 12)
+        self.assertEqual(indexes['i5'], 12)
+
+        indexes = get_index_intervals(320,-454,8850,1)
+        self.assertEqual(indexes['i0'], 320)
+        self.assertEqual(indexes['i1'], 320)
+        self.assertEqual(indexes['i2'], 318)
+        self.assertEqual(indexes['i3'], 314)
+        self.assertEqual(indexes['i4'], 314)
+        self.assertEqual(indexes['i5'], 314)
+        self.assertEqual(indexes['i6'], 314)
+        self.assertEqual(indexes['i7'], 314)
+        self.assertEqual(indexes['i8'], 314)
+        self.assertEqual(indexes['i9'], 58)
+        self.assertEqual(indexes['i10'], -454)
+        self.assertEqual(indexes['i11'], -454)
+        self.assertEqual(indexes['i12'], -454)
+        self.assertEqual(indexes['i13'], -454)
+        self.assertEqual(indexes.has_key('i14'), False)
+
+        indexes = get_index_intervals(320,-454,8850,10)
+        self.assertEqual(indexes['i0'], 32)
+        self.assertEqual(indexes['i1'], 32)
+        self.assertEqual(indexes['i2'], 30)
+        self.assertEqual(indexes['i3'], 26)
+        self.assertEqual(indexes['i4'], 18)
+        self.assertEqual(indexes['i5'], 18)
+        self.assertEqual(indexes['i6'], 18)
+        self.assertEqual(indexes['i7'], -46)
+        self.assertEqual(indexes['i8'], -46)
+        self.assertEqual(indexes['i9'], -46)
+        self.assertEqual(indexes.has_key('i10'), False)
+
+    def test_query_intervals(self):
+        intervals = get_query_intervals(-454, 8850, 315, 330)
+        self.assertEqual(intervals['i0'], [315])
+        self.assertEqual(intervals['i1'], [316])
+        self.assertEqual(intervals['i2'], [318])
+        self.assertEqual(intervals['i3'], [322])
+
+        intervals = get_query_intervals(-454, 8850, 315, 329)
+        self.assertEqual(intervals['i0'], [315,328])
+        self.assertEqual(intervals['i1'], [316,326])
+        self.assertEqual(intervals['i2'], [318,322])
+        self.assertEqual(intervals.has_key('i3'), False)
+
+        intervals = get_query_intervals(-20, 40, 10, 20)
+        self.assertEqual(intervals.has_key('i0'), False)
+        self.assertEqual(intervals['i1'], [10])
+        self.assertEqual(intervals.has_key('i2'), False)
+        self.assertEqual(intervals['i3'], [12])
+    
+        intervals = get_query_intervals(-20, 40, -21, 41)
+        self.assertEqual(intervals.has_key('i0'), False)
+        self.assertEqual(intervals.has_key('i1'), False)
+        self.assertEqual(intervals['i2'], [36])
+        self.assertEqual(intervals['i3'], [28])
+        self.assertEqual(intervals['i4'], [12])
+        self.assertEqual(intervals['i5'], [-20])
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    unittest.main()
+
